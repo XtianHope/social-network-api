91d62618d98de5604f4781df27572298d444a00a
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-04 21:10:21 -0500

    setting up models/user.js

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new mongoose.Schema(
+    {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
