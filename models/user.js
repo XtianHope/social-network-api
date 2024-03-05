acdbaf404a57d82ac8a0fbae20c6860833dc3584
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-04 21:14:41 -0500

    user.js continued

--- models/user.js
+++ models/user.js
@@ -18,3 +18,11 @@ const userSchema = new mongoose.Schema(
     thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
