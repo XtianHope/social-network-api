d697683777fec24817416f7f67e627539a9eb9fc
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-04 21:34:21 -0500

    worked more on server.js

--- server.js
+++ server.js
@@ -22,3 +22,45 @@ app.get('/api/users', async (req, res) => {
   const users = await User.find();
   res.json(users);
 });
+
+app.get('/api/users/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id).populate('thoughts');
+      res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+        }
+});
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.put('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id , req.body, {new: true});
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+});
