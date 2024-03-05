9d3908e45836fe3f3866c85ae723cb53434a1ebf
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-04 21:25:59 -0500

    began working on thought.js

---
+++ models/thought.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+        get: timestamp => dateFormat(timestamp).toLocaleString()
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
