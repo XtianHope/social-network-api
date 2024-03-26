e83d3b776d3cb78da40c83a2d6f1f70a8a608b6e
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-25 21:17:03 -0400

    completed thoughtController by adding the ability to get thought by id, create a new thought, update the thought by id, and delete a thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,3 +10,60 @@ const thoughtController = {
         }
     },
 
+// Get Thought by ID
+    getThoughtById: async (req, res) => {
+        try {
+            const thought = await Thought.findById(req.params.thoughtId );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+// Create New Thought
+    createThought: async (req, res) => {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+// Update Thought by ID
+    updateThought: async (req, res) => {
+        try {
+            const thought = await Thought.findByIdAndUpdate(
+                req.params.thoughtId,
+                req.body,
+                { new: true }
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            } else {
+                res.json(thought);
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+// Delete Thought by ID
+    deleteThought: async (req, res) => {
+        try {
+            const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            } else {
+                res.json(thought);
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+};
+
+module.exports = thoughtController;
