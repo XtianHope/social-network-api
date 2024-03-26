5c0ef2b717e8a3bed7fddc839ec6c92d22a2c530
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-03-25 21:10:30 -0400

    defined thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,12 @@
 const Thought = require('../models/thought');
+
+const thoughtController = {
+    getAllThoughts: async (req, res) => {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
